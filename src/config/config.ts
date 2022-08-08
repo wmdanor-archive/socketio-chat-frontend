880c31fa1b725ccc452110e22ee548627043dd73
wmdanor <wmdanor@gmail.com>
2022-08-08 20:10:13 +0100

    Resolve "Property 'env' does not exist on type 'ImportMeta'." issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,5 @@
 import { AppConfig } from './types';
 
-// TODO: resolve "Property 'env' does not exist on type 'ImportMeta'." issue
-// ts-ignore temporary used to fix this issue
-
 export const config: AppConfig = {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   backendHost: import.meta.env.VITE_BACKEND_HOST,
 };
--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_BACKEND_HOST: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
